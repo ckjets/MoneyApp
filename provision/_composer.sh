28d85bc5c9094052b3fcccbfc5fedf83b5d14e74
ck_jets <ck_jets@yahoo.co.jp>
2019-06-22 17:30:11 +0900

    コメント整理

--- provision/_composer.sh
+++ provision/_composer.sh
@@ -1,5 +1,9 @@
-# Composer
-echo --INSTALL Composer--
+echo -------------------------------------------------
+echo
+echo                Composer
+echo
+echo -------------------------------------------------
+
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php composer-setup.php
 php -r "unlink('composer-setup.php');"
--- provision/vagrant_user.sh
+++ provision/vagrant_user.sh
@@ -1,3 +1,9 @@
+echo -------------------------------------------------
+echo
+echo                プロジェクト設定
+echo
+echo -------------------------------------------------
+
 cd /app/cake
 composer install
 bin/cake migrations migrate
