86ed452f784c0aa24d2650a9ec1e6d4ce3b15497
ck_jets <ck_jets@yahoo.co.jp>
2019-07-19 20:35:56 +0900

    空白全部削除（r not found回避）

--- provision/_base.sh
+++ provision/_base.sh
@@ -1,14 +1,11 @@
 echo -------------------------------------------------
 echo ファイアウォール設定
 echo -------------------------------------------------
-
 systemctl start firewalld
 firewall-cmd --add-service=http --zone=public --permanent
 firewall-cmd --reload
-
 echo -------------------------------------------------
 echo SELinux無効化
 echo -------------------------------------------------
-
 setenforce 0
 sed -i 's/SELINUX=enforcing/SELINUX=disabled/' /etc/selinux/config
--- provision/vagrant_user.sh
+++ provision/vagrant_user.sh
@@ -1,7 +1,6 @@
 echo -------------------------------------------------
 echo プロジェクト設定
 echo -------------------------------------------------
-
 cd /app/cake
 composer install
 bin/cake migrations migrate
