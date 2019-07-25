b6d06d95490f0a6527d5695f77c25a922b0f95a5
ck_jets <ck_jets@yahoo.co.jp>
2019-07-25 21:00:20 +0900

    windowsでビルド成功

--- provision/vagrant_user.sh
+++ provision/vagrant_user.sh
@@ -1,7 +1,17 @@
 echo -------------------------------------------------
+echo Composer
+echo -------------------------------------------------
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+sudo mv composer.phar /usr/local/bin/composer
+composer
+
+echo -------------------------------------------------
 echo プロジェクト設定
 echo -------------------------------------------------
 cd /app/cake
+composer update
 composer install
 bin/cake migrations migrate
 bin/cake migrations seed
